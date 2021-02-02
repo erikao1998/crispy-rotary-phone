016433d14119f6c8639956280b2194d525eaf7e3
erikao1998 <59066641+erikao1998@users.noreply.github.com>
2021-02-02 20:45:33 +0200

    uses the file as the source. Prints only the beginnings of the first ten articles

--- week2/search_engine.py
+++ week2/search_engine.py
@@ -1,12 +1,22 @@
 from sklearn.feature_extraction.text import CountVectorizer
+import re
 
-documents = ["This is a silly example",
-             "A better example this",
-             "Nothing to this see here",
-             "This is a great and long example"]
+try:
+    with open(r"enwiki-20181001-corpus.100-articles.txt", encoding="utf-8") as f:
+        doc = f.read()
+        doc_split = re.split("</?article.*>", doc)
+
+except:
+    print("file cannot be read")
+
+
+# documents = ["This is a silly example",
+#             "A better example this",
+#             "Nothing to this see here",
+#             "This is a great and long example"]
 
 cv = CountVectorizer(lowercase=True, token_pattern = r"(?u)\b\w+\b", binary=True)
-sparse_matrix = cv.fit_transform(documents)
+sparse_matrix = cv.fit_transform(doc_split)
 dense_matrix = sparse_matrix.todense()
 td_matrix = dense_matrix.T
 
@@ -33,20 +43,23 @@ def test_query(query):
 
 while True:
     y = True
-    x = input("enter a word (empty line stops the program): ")
+    print("Enter a word (empty line stops the program). Put operators in capitals.")
+    x = input()
     if len(x) == 0:
         break
-    elif x not in set((" ".join(z.lower() for z in documents)).split()):
-        print("unknown word")
+    elif x not in set(re.split("\W|-", (" ".join(z.lower() for z in doc_split)))):
+        print("Unknown word")
+        print()
         y = False
     if y:
         test_query(x)
 
         hits_matrix = eval(rewrite_query(x))
-        print("Matching documents as vector (it is actually a matrix with one single row):", hits_matrix)
-        print("The coordinates of the non-zero elements:", hits_matrix.nonzero())
+        # print("Matching documents as vector (it is actually a matrix with one single row):", hits_matrix)
+        # print("The coordinates of the non-zero elements:", hits_matrix.nonzero())
 
         hits_list = list(hits_matrix.nonzero()[1])
 
-        for i, doc_idx in enumerate(hits_list):
-            print("Matching doc #{:d}: {:s}".format(i, documents[doc_idx]))
+        for i, doc_idx in enumerate(hits_list[:10]):
+            print("Matching doc #{:d}: {:.500}...".format(i+1, doc_split[doc_idx]))
+            print()
