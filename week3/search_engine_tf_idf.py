9e950ca400117f3cc09bd4d3ee697ef9e18ada9d
suvis705 <suvi.m.seppanen@helsinki.fi>
2021-02-04 12:46:14 +0200

    added search function

--- week3/search_engine_tf_idf.py
+++ week3/search_engine_tf_idf.py
@@ -1,5 +1,7 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import re
+import numpy as np
+import nltk
 
 def open_file():
     names = []
@@ -30,14 +32,27 @@ def open_file():
 #             "This is a great and long example"]
 names, doc_split = open_file()
 
-gv = TfidfVectorizer(lowercase=True, sublinear_tf=False, use_idf=False, norm=None)
-tf_matrix1 = gv.fit_transform(doc_split).T.todense()
+gv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
+g_matrix = gv.fit_transform(doc_split).T.tocsr()
 
-print("TfidfVectorizer:")
-print(tf_matrix1)
+def search_article(query_string):
 
-print("\nCountVectorizer:")
-print(dense_matrix)
+    # Vectorize query string
+    query_vec = gv.transform([ query_string ]).tocsc()
+
+    # Cosine similarity
+    hits = np.dot(query_vec, g_matrix)
+
+    # Rank hits
+    ranked_scores_and_doc_ids = \
+        sorted(zip(np.array(hits[hits.nonzero()])[0], hits.nonzero()[1]),
+               reverse=True)
+
+    # Output result
+    print("Your query '{:s}' matches the following documents:".format(query_string))
+    for i, (score, doc_idx) in enumerate(ranked_scores_and_doc_ids):
+        print("Doc #{:d} (score: {:.4f}): {:s}".format(i, score, names[doc_idx]))
+    print()
 
 sparse_matrix = cv.fit_transform(doc_split)
 dense_matrix = sparse_matrix.todense()
@@ -70,12 +85,13 @@ while True:
             print()
             x = False
     if x:
-        hits_matrix = eval(rewrite_query(inp))
-
-        hits_list = list(hits_matrix.nonzero()[1])
-
-        print("The word is in ", len(hits_list), "articles")
-        for i, doc_idx in enumerate(hits_list[:10]):
-            print("The name of the article {}: {}".format(i+1, names[(int(doc_idx))]))
-            print("Preview: {:.500}...".format(doc_split[doc_idx]))
-            print()
+        search_article(inp)
+        # hits_matrix = eval(rewrite_query(inp))
+        #
+        # hits_list = list(hits_matrix.nonzero()[1])
+        #
+        # print("The word is in ", len(hits_list), "articles")
+        # for i, doc_idx in enumerate(hits_list[:10]):
+        #     print("The name of the article {}: {}".format(i+1, names[(int(doc_idx))]))
+        #     print("Preview: {:.500}...".format(doc_split[doc_idx]))
+        #     print()
